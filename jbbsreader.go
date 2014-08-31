d060c5d672350f21f74abc7398e449e9a6c68f88
mogepiyo <coffee.potato+mogepiyo@gmail.com>
2014-08-31 14:41:20 +0000

    gofmt files

--- jbbsreader.go
+++ jbbsreader.go
@@ -3,192 +3,191 @@
 package jbbsreader
 
 import (
-  "fmt"
-  "net/http"
-  "io"
-  "bufio"
-  "regexp"
-  "strconv"
-  "code.google.com/p/go.text/transform"
-  "code.google.com/p/go.text/encoding/japanese"
+	"bufio"
+	"code.google.com/p/go.text/encoding/japanese"
+	"code.google.com/p/go.text/transform"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
 )
 
 // The URL formats to send requests to.
 const (
-  jbbsURLFormat = "http://jbbs.shitaraba.net/%s/%s/"
-  jbbsSubjectURLFormat = "http://jbbs.shitaraba.net/%s/%s/subject.txt"
-  jbbsDatURLFormat = "http://jbbs.shitaraba.net/bbs/rawmode.cgi/%s/%s/%s"
+	jbbsURLFormat        = "http://jbbs.shitaraba.net/%s/%s/"
+	jbbsSubjectURLFormat = "http://jbbs.shitaraba.net/%s/%s/subject.txt"
+	jbbsDatURLFormat     = "http://jbbs.shitaraba.net/bbs/rawmode.cgi/%s/%s/%s"
 )
 
 // The regexps to parse dat from JBBS.
 const (
-  subjectPattern = `^([[:digit:]]*)\.cgi,(.*)\(([[:digit:]]*)\)$`
-  datPattern = `^([[:digit:]]*)<>(.*)<>(.*)<>(.*)<>(.*)<>(.*)<>(.*)$`
+	subjectPattern = `^([[:digit:]]*)\.cgi,(.*)\(([[:digit:]]*)\)$`
+	datPattern     = `^([[:digit:]]*)<>(.*)<>(.*)<>(.*)<>(.*)<>(.*)<>(.*)$`
 )
 
 var (
-  subjectRegexp = regexp.MustCompile(subjectPattern)
-  datRegexp = regexp.MustCompile(datPattern)
+	subjectRegexp = regexp.MustCompile(subjectPattern)
+	datRegexp     = regexp.MustCompile(datPattern)
 )
 
 var datEncoding = japanese.EUCJP
 
 // Board represents a JBBS board whose URL is 'http://jbbs.shitaraba.net/[category]/[id]/'.
 type Board struct {
-  URL string
-  Category, ID string
+	URL          string
+	Category, ID string
 }
 
 // NewBoard constructs a Board specified by category and id.
-func NewBoard (category, id string) *Board {
-  return &Board{
-    URL: fmt.Sprintf(jbbsURLFormat, category, id),
-    Category: category,
-    ID: id,
-  }
+func NewBoard(category, id string) *Board {
+	return &Board{
+		URL:      fmt.Sprintf(jbbsURLFormat, category, id),
+		Category: category,
+		ID:       id,
+	}
 }
 
 // Threads returns the list of threads in the board.
 func (b *Board) Threads() (threads []*Thread, _ error) {
-  subjectURL := fmt.Sprintf(jbbsSubjectURLFormat, b.Category, b.ID)
+	subjectURL := fmt.Sprintf(jbbsSubjectURLFormat, b.Category, b.ID)
 
-  // Read from JBBS.
-  lines, err := getLines(subjectURL)
-  if err != nil {
-    return nil, err
-  }
+	// Read from JBBS.
+	lines, err := getLines(subjectURL)
+	if err != nil {
+		return nil, err
+	}
 
-  // Parse each line.
-  for i, line := range lines {
-    t := newThread(b, line);
-    if t == nil {
-      return nil, fmt.Errorf("could not parse subject.txt:%v %q", i, line)
-    }
-    threads = append(threads, t)
-  }
+	// Parse each line.
+	for i, line := range lines {
+		t := newThread(b, line)
+		if t == nil {
+			return nil, fmt.Errorf("could not parse subject.txt:%v %q", i, line)
+		}
+		threads = append(threads, t)
+	}
 
-  return threads, nil
+	return threads, nil
 }
 
 // Thread represents the thread that can be accessed by the following URL.
 // 'http://jbbs.shitaraba.net/bbs/read.cgi/[board-category]/[board-id]/[thread-id]'
 type Thread struct {
-  parentBoard *Board
-  ID string
-  Title string // The title of the thread.
-  NumResponses uint // The number of responses in the thread.
+	parentBoard  *Board
+	ID           string
+	Title        string // The title of the thread.
+	NumResponses uint   // The number of responses in the thread.
 }
 
 // newThread parses each line of subjects.txt and constructs a Thread, or returns nil on error.
 func newThread(parent *Board, line string) *Thread {
-  parts := subjectRegexp.FindStringSubmatch(line)
-  if parts == nil {
-    return nil
-  }
+	parts := subjectRegexp.FindStringSubmatch(line)
+	if parts == nil {
+		return nil
+	}
 
-  // Parse the number of responses.
-  numResponses, err := strconv.ParseUint(parts[3], 10, 32)
-  if err != nil {
-    return nil
-  }
+	// Parse the number of responses.
+	numResponses, err := strconv.ParseUint(parts[3], 10, 32)
+	if err != nil {
+		return nil
+	}
 
-  return &Thread{
-    parentBoard: parent,
-    ID: parts[1],
-    Title: parts[2],
-    NumResponses: uint(numResponses),
-  }
+	return &Thread{
+		parentBoard:  parent,
+		ID:           parts[1],
+		Title:        parts[2],
+		NumResponses: uint(numResponses),
+	}
 }
 
 // Responses gets all responses in the thread.
 func (t *Thread) Responses() (responses []*Response, _ error) {
-  datURL := fmt.Sprintf(jbbsDatURLFormat, t.parentBoard.Category, t.parentBoard.ID, t.ID)
+	datURL := fmt.Sprintf(jbbsDatURLFormat, t.parentBoard.Category, t.parentBoard.ID, t.ID)
 
-  // Read from JBBS.
-  lines, err := getLines(datURL)
-  if err != nil {
-    return nil, err
-  }
+	// Read from JBBS.
+	lines, err := getLines(datURL)
+	if err != nil {
+		return nil, err
+	}
 
-  // Parse each line.
-  for i, line := range lines {
-    r := newResponse(t, line);
-    if r == nil {
-      return nil, fmt.Errorf("could not parse dat l:%v %q", i, line)
-    }
-    responses = append(responses, r)
-  }
+	// Parse each line.
+	for i, line := range lines {
+		r := newResponse(t, line)
+		if r == nil {
+			return nil, fmt.Errorf("could not parse dat l:%v %q", i, line)
+		}
+		responses = append(responses, r)
+	}
 
-  return responses, nil
+	return responses, nil
 }
 
 // Response represents each response written to JBBS threads.
 type Response struct {
-  parentThread *Thread
-  ID uint // The ID of the response, which is the same as the response number.
-  Name string // The name of the author.
-  Email string // The email address of the author.
-  Date string // The date the response was made. TODO(mogepiyo): Parse the date.
-  Content string // The content of the response. TODO(mogepiyo): Strip all tags, e.g. <br>.
-  threadTitle string // The title of the parent thread.
-  AuthorID string // The ID of the author.
+	parentThread *Thread
+	ID           uint   // The ID of the response, which is the same as the response number.
+	Name         string // The name of the author.
+	Email        string // The email address of the author.
+	Date         string // The date the response was made. TODO(mogepiyo): Parse the date.
+	Content      string // The content of the response. TODO(mogepiyo): Strip all tags, e.g. <br>.
+	threadTitle  string // The title of the parent thread.
+	AuthorID     string // The ID of the author.
 }
 
 func newResponse(parent *Thread, line string) *Response {
-  parts := datRegexp.FindStringSubmatch(line)
-  if parts == nil {
-    return nil
-  }
+	parts := datRegexp.FindStringSubmatch(line)
+	if parts == nil {
+		return nil
+	}
 
-  // Parse the number of the response.
-  id, err := strconv.ParseUint(parts[1], 10, 32)
-  if err != nil {
-    return nil
-  }
+	// Parse the number of the response.
+	id, err := strconv.ParseUint(parts[1], 10, 32)
+	if err != nil {
+		return nil
+	}
 
-  return &Response{
-    parentThread: parent,
-    ID: uint(id),
-    Name: parts[2],
-    Email: parts[3],
-    Date: parts[4],
-    Content: parts[5],
-    threadTitle: parts[6],
-    AuthorID: parts[7],
-  }
+	return &Response{
+		parentThread: parent,
+		ID:           uint(id),
+		Name:         parts[2],
+		Email:        parts[3],
+		Date:         parts[4],
+		Content:      parts[5],
+		threadTitle:  parts[6],
+		AuthorID:     parts[7],
+	}
 }
 
 // getLines sends a HTTP GET request to the url, and returns a slice of lines of the response.
 // This is a var so to stub it in tests.
 var getLines = func(url string) ([]string, error) {
-  resp, err := http.Get(url)
-  if err != nil {
-    return nil, err
-  }
-  defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-  // Read and transform to UTF-8 encoding.
-  r := transform.NewReader(resp.Body, datEncoding.NewDecoder())
-  ls, err := lines(r)
-  if err != nil {
-    return nil, err
-  }
+	// Read and transform to UTF-8 encoding.
+	r := transform.NewReader(resp.Body, datEncoding.NewDecoder())
+	ls, err := lines(r)
+	if err != nil {
+		return nil, err
+	}
 
-  return ls, nil
+	return ls, nil
 }
 
 // lines reads each non-empty line from r, with no EOL markers such as CR, LF, and CR+LF.
 func lines(r io.Reader) (ls []string, _ error) {
-  scanner := bufio.NewScanner(r)
-  for scanner.Scan() {
-    l := scanner.Text()
-    if (len(l) > 0) {
-      ls = append(ls, l)
-    }
-  }
-  if err := scanner.Err(); err != nil {
-    return nil, err
-  }
-  return ls, nil
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		l := scanner.Text()
+		if len(l) > 0 {
+			ls = append(ls, l)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return ls, nil
 }
-
--- jbbsreader_test.go
+++ jbbsreader_test.go
@@ -1,167 +1,167 @@
 package jbbsreader
 
 import (
-  "testing"
-  "reflect"
+	"reflect"
+	"testing"
 )
 
 func TestNewBoard(t *testing.T) {
-  got := NewBoard("computer", "12345").URL
-  if want := "http://jbbs.shitaraba.net/computer/12345/"; got != want {
-    t.Errorf("got %v, want %v", got, want)
-  }
+	got := NewBoard("computer", "12345").URL
+	if want := "http://jbbs.shitaraba.net/computer/12345/"; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
 
 func TestThreadsURL(t *testing.T) {
-  restore := swapGetLines(func(url string) ([]string, error) {
-    if want := "http://jbbs.shitaraba.net/computer/12345/subject.txt"; url != want {
-      t.Errorf("URL: got %v, want %v", url, want)
-    }
-    return []string{"1.cgi,title(10)"}, nil
-  })
-  defer restore()
+	restore := swapGetLines(func(url string) ([]string, error) {
+		if want := "http://jbbs.shitaraba.net/computer/12345/subject.txt"; url != want {
+			t.Errorf("URL: got %v, want %v", url, want)
+		}
+		return []string{"1.cgi,title(10)"}, nil
+	})
+	defer restore()
 
-  b := NewBoard("computer", "12345")
-  b.Threads()
+	b := NewBoard("computer", "12345")
+	b.Threads()
 }
 
 func TestThreads(t *testing.T) {
-  b := NewBoard("computer", "12345")
+	b := NewBoard("computer", "12345")
 
-  tcs := []struct {
-    desc string
-    in string
-    want *Thread
-    wantErr bool
-  }{{
-    desc: "complex",
-    in: "999.cgi,hoge.cgi,(20)fuga(42)",
-    want: &Thread{
-      parentBoard: b,
-      ID: "999",
-      Title: "hoge.cgi,(20)fuga",
-      NumResponses: 42,
-    },
-  }, {
-    desc: "invalid separator",
-    in: "999.cgihog,(20)",
-    wantErr: true,
-  }, {
-    desc: "numResponses not parseable",
-    in: "999.cgi,(2a)",
-    wantErr: true,
-  }}
+	tcs := []struct {
+		desc    string
+		in      string
+		want    *Thread
+		wantErr bool
+	}{{
+		desc: "complex",
+		in:   "999.cgi,hoge.cgi,(20)fuga(42)",
+		want: &Thread{
+			parentBoard:  b,
+			ID:           "999",
+			Title:        "hoge.cgi,(20)fuga",
+			NumResponses: 42,
+		},
+	}, {
+		desc:    "invalid separator",
+		in:      "999.cgihog,(20)",
+		wantErr: true,
+	}, {
+		desc:    "numResponses not parseable",
+		in:      "999.cgi,(2a)",
+		wantErr: true,
+	}}
 
-  original := getLines;
-  defer func() { getLines = original }()
+	original := getLines
+	defer func() { getLines = original }()
 
-  for _, tc := range tcs {
-    swapGetLines(func(url string) ([]string, error) {
-      return []string{tc.in}, nil
-    })
-    threads, err := b.Threads()
-    if err != nil {
-      if (!tc.wantErr) {
-        t.Errorf("%s: err: %v", tc.desc, err)
-      }
-      continue;
-    }
-    if len(threads) != 1 {
-      t.Errorf("%s: len(threads) %v, want 1", tc.desc, len(threads))
-      continue;
-    }
-    got := threads[0]
-    if !reflect.DeepEqual(got, tc.want) {
-      t.Errorf("%s: got %v, want %v", tc.desc, got, tc.want)
-    }
-  }
+	for _, tc := range tcs {
+		swapGetLines(func(url string) ([]string, error) {
+			return []string{tc.in}, nil
+		})
+		threads, err := b.Threads()
+		if err != nil {
+			if !tc.wantErr {
+				t.Errorf("%s: err: %v", tc.desc, err)
+			}
+			continue
+		}
+		if len(threads) != 1 {
+			t.Errorf("%s: len(threads) %v, want 1", tc.desc, len(threads))
+			continue
+		}
+		got := threads[0]
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("%s: got %v, want %v", tc.desc, got, tc.want)
+		}
+	}
 }
 
 func TestResponsesURL(t *testing.T) {
-  restore := swapGetLines(func(url string) ([]string, error) {
-    if want := "http://jbbs.shitaraba.net/bbs/rawmode.cgi/computer/12345/67890"; url != want {
-      t.Errorf("URL: got %v, want %v", url, want)
-    }
-    return []string{"1<><><><><><>"}, nil
-  })
-  defer restore()
+	restore := swapGetLines(func(url string) ([]string, error) {
+		if want := "http://jbbs.shitaraba.net/bbs/rawmode.cgi/computer/12345/67890"; url != want {
+			t.Errorf("URL: got %v, want %v", url, want)
+		}
+		return []string{"1<><><><><><>"}, nil
+	})
+	defer restore()
 
-  b := NewBoard("computer", "12345")
-  th := &Thread{
-    parentBoard: b,
-    ID: "67890",
-  }
-  th.Responses()
+	b := NewBoard("computer", "12345")
+	th := &Thread{
+		parentBoard: b,
+		ID:          "67890",
+	}
+	th.Responses()
 }
 
 func TestResponses(t *testing.T) {
-  b := NewBoard("computer", "12345")
-  th := &Thread{
-    parentBoard: b,
-    ID: "67890",
-  }
+	b := NewBoard("computer", "12345")
+	th := &Thread{
+		parentBoard: b,
+		ID:          "67890",
+	}
 
-  tcs := []struct {
-    desc string
-    in string
-    want *Response
-    wantErr bool
-  }{{
-    desc: "id not parseable",
-    in: "abc<><><><><><>",
-    wantErr: true,
-  }, {
-    desc: "partial data",
-    in: "1<><><><>",
-    wantErr: true,
-  }, {
-    desc: "full",
-    in: "123<>name<>mail<>date<>content<>title<>id",
-    want: &Response{
-      parentThread: th,
-      ID: 123,
-      Name: "name",
-      Email: "mail",
-      Date: "date",
-      Content: "content",
-      threadTitle: "title",
-      AuthorID: "id",
-    },
-  }, {
-    desc: "zero",
-    in: "0<><><><><><>",
-    want: &Response{
-      parentThread: th,
-    },
-  }}
+	tcs := []struct {
+		desc    string
+		in      string
+		want    *Response
+		wantErr bool
+	}{{
+		desc:    "id not parseable",
+		in:      "abc<><><><><><>",
+		wantErr: true,
+	}, {
+		desc:    "partial data",
+		in:      "1<><><><>",
+		wantErr: true,
+	}, {
+		desc: "full",
+		in:   "123<>name<>mail<>date<>content<>title<>id",
+		want: &Response{
+			parentThread: th,
+			ID:           123,
+			Name:         "name",
+			Email:        "mail",
+			Date:         "date",
+			Content:      "content",
+			threadTitle:  "title",
+			AuthorID:     "id",
+		},
+	}, {
+		desc: "zero",
+		in:   "0<><><><><><>",
+		want: &Response{
+			parentThread: th,
+		},
+	}}
 
-  original := getLines;
-  defer func() { getLines = original }()
+	original := getLines
+	defer func() { getLines = original }()
 
-  for _, tc := range tcs {
-    swapGetLines(func(url string) ([]string, error) {
-      return []string{tc.in}, nil
-    })
-    threads, err := th.Responses()
-    if err != nil {
-      if (!tc.wantErr) {
-        t.Errorf("%s: err: %v", tc.desc, err)
-      }
-      continue;
-    }
-    if len(threads) != 1 {
-      t.Errorf("%s: len(threads) %v, want 1", tc.desc, len(threads))
-      continue;
-    }
-    got := threads[0]
-    if !reflect.DeepEqual(got, tc.want) {
-      t.Errorf("%s: got %v, want %v", tc.desc, got, tc.want)
-    }
-  }
+	for _, tc := range tcs {
+		swapGetLines(func(url string) ([]string, error) {
+			return []string{tc.in}, nil
+		})
+		threads, err := th.Responses()
+		if err != nil {
+			if !tc.wantErr {
+				t.Errorf("%s: err: %v", tc.desc, err)
+			}
+			continue
+		}
+		if len(threads) != 1 {
+			t.Errorf("%s: len(threads) %v, want 1", tc.desc, len(threads))
+			continue
+		}
+		got := threads[0]
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("%s: got %v, want %v", tc.desc, got, tc.want)
+		}
+	}
 }
 
 func swapGetLines(newSwap func(url string) ([]string, error)) func() {
-  orig := getLines
-  getLines = newSwap
-  return func() { getLines = orig }
+	orig := getLines
+	getLines = newSwap
+	return func() { getLines = orig }
 }
